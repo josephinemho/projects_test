0f097bd489e0a63ed9b70d45071ca4bdf9359216
josephinemho@gmail.com <josephinemho@gmail.com>
2021-02-07 00:03:09 +0000

    Updated content

--- test_notebook.py
+++ test_notebook.py
@@ -1,5 +1,12 @@
 # Databricks notebook source
-print("this is a test")
+# MAGIC %md 
+# MAGIC # Projects 
+# MAGIC #### This demo covers:
+# MAGIC - Creating new folders (repos) in the Projects tab
+# MAGIC - Managing permissions
+# MAGIC - Git: create new branches, pull, view history, etc.
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ####Projects demo from NOTEBOOK
